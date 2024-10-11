59e254e80f9c10f611fe71fe073826056bb3b13d
Ana Flávia V. Kunzendorff <142523426+akunzendorff@users.noreply.github.com>
2024-10-10 22:30:20 -0300

    Add files via upload

---
+++ MiteScan/src/Model/Colmeia.java
@@ -0,0 +1,123 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Model;
+
+import Control.Conexao;
+import java.sql.ResultSet;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author fatec-dsm2
+ */
+public class Colmeia {
+    private int codigo;
+    private String nome;
+    private String locLat;
+    private String locLong;
+    private String tamanho;
+    private String tipoAbelha;
+    
+    Conexao con = new Conexao();
+
+    public Colmeia() {
+        this(0, "", "", "", "", "");
+    }
+
+    public Colmeia(int codigo, String nome, String locLat, String locLong, String tamanho, String tipoAbelha) {
+        this.codigo = codigo;
+        this.nome = nome;
+        this.locLat = locLat;
+        this.locLong = locLong;
+        this.tamanho = tamanho;
+        this.tipoAbelha = tipoAbelha;
+    }
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getLocLat() {
+        return locLat;
+    }
+
+    public void setLocLat(String locLat) {
+        this.locLat = locLat;
+    }
+
+    public String getLocLong() {
+        return locLong;
+    }
+
+    public void setLocLong(String locLong) {
+        this.locLong = locLong;
+    }
+
+    public String getTamanho() {
+        return tamanho;
+    }
+
+    public void setTamanho(String tamanho) {
+        this.tamanho = tamanho;
+    }
+
+    public String getTipoAbelha() {
+        return tipoAbelha;
+    }
+
+    public void setTipoAbelha(String tipoAbelha) {
+        this.tipoAbelha = tipoAbelha;
+    }
+    
+    public void cadastrarUsuario(){
+        String sql = "Insert into colmeia (codigo, nome, email, login, senha, acesso) values " +
+                "(" + getCodigo() + ",'" + getNome() + "','" + getLocLat()
+                + "','" + getLocLong() + "','" + getTamanho() + "', " + getTipoAbelha() + " )";
+        
+        con.executeSQL(sql);
+        JOptionPane.showMessageDialog(null, "Registrado com sucesso!");  
+    }
+    
+    public ResultSet listarUsuario(){
+       ResultSet tabela;
+       tabela = null;
+        
+        String sql= "Select * from colmeia";
+        tabela = con.RetornarResultset(sql);
+     return tabela;
+    }
+    
+    public void excluirUsuario(){
+        String sql;
+        sql = "Delete from colmeia where codigo = " + getCodigo();
+        
+        con.executeSQL(sql);
+        JOptionPane.showMessageDialog(null, "Registro excluido com sucesso.");
+    }
+    
+    public void editarUsuario(){
+        String sql;
+        sql = "Update usuario set nome = '" + getNome() + "', loclat = '" + getLocLat()
+                + "', loclong = '" + getLocLong() + "', tamanho = '" + getTamanho()
+                + "', tipoAbelha = " + getTipoAbelha() + " where codigo = " + getCodigo();
+        
+        con.executeSQL(sql);
+        JOptionPane.showMessageDialog(null, "Registro alterado com sucesso!");
+    }
+    
+}
---
+++ MiteScan/src/Model/Usuario.java
@@ -0,0 +1,122 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Model;
+
+import Control.Conexao;
+import java.sql.ResultSet;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author fatec-dsm2
+ */
+public class Usuario {
+    
+    private int codigo;
+    private String nome;
+    private String email;
+    private String login;
+    private String senha;
+    private String acesso;
+    
+    Conexao con = new Conexao();
+
+    public Usuario() {
+        this(0, "", "", "", "", "");
+    }
+
+    public Usuario(int codigo, String nome, String email, String login, String senha, String acesso) {
+        this.codigo = codigo;
+        this.nome = nome;
+        this.email = email;
+        this.login = login;
+        this.senha = senha;
+        this.acesso = acesso;
+    }
+    
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public String getAcesso() {
+        return acesso;
+    }
+
+    public void setAcesso(String acesso) {
+        this.acesso = acesso;
+    }
+    
+    public void cadastrarUsuario(){
+        String sql = "Insert into usuario (codigo, nome, email, login, senha, acesso) values " +
+                "(" + getCodigo() + ",'" + getNome() + "','" + getEmail()
+                + "','" + getLogin() + "','" + getSenha() + "', " + getAcesso() + " )";
+        
+        con.executeSQL(sql);
+        JOptionPane.showMessageDialog(null, "Registrado com sucesso!");  
+    }
+    
+    public ResultSet listarUsuario(){
+       ResultSet tabela;
+       tabela = null;
+        
+        String sql= "Select * from usuario";
+        tabela = con.RetornarResultset(sql);
+     return tabela;
+    }
+    
+    public void excluirUsuario(){
+        String sql;
+        sql = "Delete from usuario where codigo = " + getCodigo();
+        con.executeSQL(sql);
+        JOptionPane.showMessageDialog(null, "Registro excluido com sucesso.");
+    }
+    
+    public void editarUsuario(){
+        String sql;
+        sql = "Update usuario set nome = '" + getNome() + "', email = '" + getEmail()
+                + "', login = '" + getLogin() + "', senha = '" + getSenha()
+                + "', acesso = " + getAcesso() + " where codigo = " + getCodigo();
+        
+        con.executeSQL(sql);
+        JOptionPane.showMessageDialog(null, "Registro alterado com sucesso!");
+    }
+}
